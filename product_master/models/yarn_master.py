8f228c71646d5f1dd5dfca0b5653a461e697cbe2
Umair-Abdullah <umairabdullah2512@gmail.com>
2022-11-14 15:03:40 +0500

    master product fix
    master product fix

--- product_master/models/yarn_master.py
+++ product_master/models/yarn_master.py
@@ -109,11 +109,11 @@ class yarnProductCategoryProductTemplate(models.Model):
         required=True)
 
     item_name = fields.Char(string='Item Name', )
-    name = fields.Char(required=False, related='name_temp')
-    name_temp = fields.Char(compute='createYarnName')
+    # name = fields.Char(related='name_temp')
+    # name_temp = fields.Char(default='hh')
 
     sub_categ_id = fields.Many2one('product.category', 'Item Category', )
-    yarn_sub_categ_id = fields.Many2one('product.category', 'Yarn Category',)
+    yarn_sub_categ_id = fields.Many2one('product.category', 'Yarn Category', )
 
     code = fields.Char(string='Yarn Code', compute='YarnCode')
     code_temp = fields.Char()
@@ -128,11 +128,11 @@ class yarnProductCategoryProductTemplate(models.Model):
     yarn_level = fields.Many2one('yarn.level')
     yarn_weaving_loom = fields.Many2one('yarn.weaving.loom')
 
-    @api.model
-    def create(self, vals):
-        record = super(yarnProductCategoryProductTemplate, self).create(vals)
-        record['code_temp'] = self.env['ir.sequence'].next_by_code('product_code_seq') or ('New')
-        return record
+    # @api.model
+    # def create(self, vals):
+    #     record = super(yarnProductCategoryProductTemplate, self).create(vals)
+    #     record['code_temp'] = self.env['ir.sequence'].next_by_code('product_code_seq') or ('New')
+    #     return record
 
     def YarnCode(self):
         yarn_code = str(self.item_category.code) + '-' + str(self.item_group.code) + '-' + str(self.code_temp)
@@ -142,28 +142,25 @@ class yarnProductCategoryProductTemplate(models.Model):
     # def ProductName(self):
     #     self.name = self.name_temp
 
-    # @api.onchange('master_product', 'item_group', 'greige_dyed', 'yarn_composition', 'yarn_structure', 'yarn_count',
-    #               'yarn_category')
+    @api.onchange('master_product', 'item_group', 'greige_dyed', 'yarn_composition', 'yarn_structure', 'yarn_count',
+                  'yarn_category', 'item_name')
     def createYarnName(self):
-        try:
-            if self.master_product == 'yarn_master':
-                item_group = self.item_group.name
-                item_category = self.item_category.name
-                greige_dyed = self.greige_dyed.name
-                yarn_count = self.yarn_count.name
-                yarn_structure = self.yarn_structure.name
-                yarn_composition = self.yarn_composition.name
-                yarn_category = self.yarn_category.name
+        if self.master_product == 'yarn_master':
+            item_group = self.item_group.name
+            item_category = self.item_category.name
+            greige_dyed = self.greige_dyed.name
+            yarn_count = self.yarn_count.name
+            yarn_structure = self.yarn_structure.name
+            yarn_composition = self.yarn_composition.name
+            yarn_category = self.yarn_category.name
 
-                name = str(item_group) + ' ' + str(greige_dyed) + '- ' + str(yarn_composition) + ' ' + str(
-                    yarn_structure) + '-' + str(
-                    yarn_count) + ' ' + str(yarn_category)
-                self.name_temp = name
+            name = str(item_group) + ' ' + str(greige_dyed) + '- ' + str(yarn_composition) + ' ' + str(
+                yarn_structure) + '-' + str(
+                yarn_count) + ' ' + str(yarn_category)
+            self.name = name
 
-            else:
-                self.name_temp = self.item_name
-        except:
-            pass
+        else:
+            self.name = self.item_name
 
     # @api.onchange('yarn_name','master_product')
     # def productNameYarn(self):
